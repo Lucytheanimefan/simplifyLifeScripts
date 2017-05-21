92bcbe7bd764d7d6f31f51aaa585bf992808b767
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-05-20 21:12:47 -0700

    get studio

--- new_anime_rec.py
+++ new_anime_rec.py
@@ -6,6 +6,7 @@ import requests
 aniChartUrl="https://www.livechart.me/"
 
 def findSeasonRecs():
+	season_anime = {}
 	print "Anime season (summer, spring, winter, or fall)"
 	season = sys.stdin.readline().strip()
 	url = aniChartUrl + season+"-2017/tv"
@@ -16,9 +17,20 @@ def findSeasonRecs():
 	soup = BeautifulSoup(data,  "html.parser")
 	titles = soup.find_all("div", {"class": "anime-card"})
 	for title in titles:
-		alink = title.find_all("h3",{"class":"main-title"})
-		for link in alink:
-			print link.text
+		alink = title.find_all("h3",{"class":"main-title"})[0]
+		print(alink.text)
+		tags = [tag.text for tag in title.find_all("ol",{"class":"anime-tags"})[0].find_all("li")]
+		season_anime[alink.text] = {"tags":tags}
+		studios = title.find_all("ul",{"class":"anime-studios"})
+		#print(studios)
+		parsed_studios = []
+		for studio in studios:
+			if studio.find_all("a"):
+				stud = studio.find_all("a")[0].text
+				parsed_studios.append(stud)
+			else:
+				parsed_studios.append(studio.find_all("li")[0].text)
+		print(parsed_studios)
 
 
 
