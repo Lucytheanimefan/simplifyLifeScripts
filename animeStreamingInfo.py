300d7275e1c1f449e8c7df6f5fd3da871838b95e
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-06-30 22:43:51 -0700

    fix to get crunchyroll licensed shows

--- animeStreamingInfo.py
+++ animeStreamingInfo.py
@@ -11,30 +11,33 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 mal_url = "https://myanimelist.net/animelist/"
 
 
-def get_anime_stream_site(anime_url):
+def get_anime_stream_site(anime_object):
+	anime_url = anime_object["anime_url"]
+	anime_title = anime_object["anime_title"]
 	print anime_url
+	to_return = {}
 	ani_req = requests.get("https://myanimelist.net" + anime_url,verify=False)
 	soup = BeautifulSoup(ani_req.text, "html.parser")
-	if "crunchyroll" in soup.find_all("ul", {"class":"anime-slide"})[0]:
-		to_return[anime["anime_title"]] = "crunchyroll"
-		print to_return
+	episodes = str(soup.find_all("ul", {"class":"anime-slide"}))
+	if len(episodes)>0 and "crunchyroll" in episodes:
+		to_return[anime_object["anime_title"]] = "crunchyroll"
+	return to_return
 
 
 def main():
     print("MAL username? ")
     username = sys.stdin.readline().strip()
-    print(mal_url + username + "?status=6")
-    r = requests.get(mal_url + username, verify=False)
+    url = mal_url + username + "?status=6"
+    r = requests.get(url, verify=False)
     data = r.text
+    print r
     soup = BeautifulSoup(data,  "html.parser")
     plan_to_watch = json.loads(soup.find_all("table", {"class": "list-table"})[0]["data-items"])
     to_return = {}
-    anime_urls = [anime["anime_url"] for anime in plan_to_watch]
     pool = Pool()                         # Create a multiprocessing Pool
-    pool.map(get_anime_stream_site, anime_urls) 
-    
+    results = pool.map(get_anime_stream_site, plan_to_watch) 
+    print results
 
-    print to_return
 
 
 
