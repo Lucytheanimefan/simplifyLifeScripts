a3d6e9d9de113bf0522dbfd9bca6e9d31a73ea8a
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-08-16 22:40:29 -0700

    Script fix

--- specific-anime.py
+++ specific-anime.py
@@ -54,6 +54,48 @@ template = { "notify":True,"message":"foo","card":{
   }
 }
 
+
+
+def set_template(anime_title, anime_url, anime_img_url, anime_description, for_whom, where_to_watch, family_friendly):
+  template = { "notify":True,"message":"foo","card":{
+  "style": "application",
+  "url": anime_url,
+  "format": "medium",
+  "id": "db797a68-0aff-4ae8-83fc-2e72dbb1a707",
+  "title": anime_title,
+  "description": anime_description,
+  "icon": {
+    "url": anime_img_url
+  },
+  "attributes": [
+    {
+      "label": "For",
+      "value": {
+        "label": for_whom
+      }
+    },
+    {
+      "label": "Where to watch?",
+      "value" : {
+        "label":where_to_watch
+      }
+    },
+    {
+      "label": "Family friendly?",
+      "value": {
+        "icon": {
+          "url": alert_img
+        },
+        "label": family_friendly,
+        "style": "lozenge-complete"
+      }
+    }
+  ]
+  }
+  }
+  return template
+
+
 def get_db():
     client = MongoClient(mongodb_uri)
     db = client[mongodb_client]
@@ -73,7 +115,8 @@ def get_new_info():
       family_friendly = new["family_friendly"]
       for_whom = new["for_whom"]
       where_to_watch = new["where_to_watch"]
-      main()
+      template = set_template(anime_title, anime_url, anime_img_url, anime_description, for_whom, where_to_watch, family_friendly)
+      main(template)
       database.applenews.update({"_id": new["_id"]}, {"$set": {"used": True}})
       return
     else:
@@ -85,7 +128,7 @@ def insert_anime():
   database.applenews.insert_one({"anime_title":"Death Note", "anime_url":"https://www.hulu.com/death-note", "anime_description":"An anime classic about a guy who finds a notebook with the ability to kill anyone whose name is written in it.", "family_friendly":"Just not for young children", "where_to_watch":"Youtube, Hulu, Netflix", "for_whom":"I recommend this show to just about everyone. Again, classic"})
 
 
-def main():
+def main(template):
   command = "curl -d '" + json.dumps(template) + "' -H 'Content-Type: application/json' " + test_url
   p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
   out, err = p.communicate()
