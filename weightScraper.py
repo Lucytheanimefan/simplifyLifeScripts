9ab24597cdb7e0d1d42256dba18c6f87ad2b4c06
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-02-15 14:51:44 -0500

    Format string

--- weightScraper.py
+++ weightScraper.py
@@ -6,7 +6,7 @@ import urllib
 
 anime_urls = ["http://typemoon.wikia.com/wiki/Shiki_Ryougi","http://ouran.wikia.com/wiki/Haruhi_Fujioka", "http://kamisamahajimemashita.wikia.com/wiki/Nanami_Momozono", "http://typemoon.wikia.com/wiki/Saber_(Fate/stay_night)", "http://ancientmagusbride.wikia.com/wiki/Chise_Hatori", "http://attackontitan.wikia.com/wiki/Mikasa_Ackerman","http://sangatsu-no-lion.wikia.com/wiki/Rei_Kiriyama", "http://horimiya.wikia.com/wiki/Hori_Kyouko", "http://horimiya.wikia.com/wiki/Miyamura_Izumi", "http://swordartonline.wikia.com/wiki/Yuuki_Asuna", "http://yahari.wikia.com/wiki/Yukino_Yukinoshita", "http://kaichouwamaidsama.wikia.com/wiki/Misaki_Ayuzawa", "http://tokyoghoul.wikia.com/wiki/Touka_Kirishima", "http://akatsukinoyona.wikia.com/wiki/Yona", "http://guiltycrown.wikia.com/wiki/Inori_Yuzuriha", "http://deathnote.wikia.com/wiki/Misa_Amane", "http://attackontitan.wikia.com/wiki/Historia_Reiss"]
 
-characters = ['Shiki Ryougi', 'Haruhi Fujioka', 'Nanami Momozono', 'Saber', 'Rei Kiriyama', 'Mikasa Ackerman', 'Hori Kyouko', 'Miyamura_Izumi', 'Yuuki Asuna', 'Yukino_Yukinoshita', 'Misaki_Ayuzawa', 'Touka_Kirishima', 'Akatsuki no Yona Yona', 'Inori_Yuzuriha', 'Misa Amane', 'Historia Reiss']
+characters = ['Shiki Ryougi', 'Haruhi Fujioka', 'Nanami Momozono', 'Saber', 'Rei Kiriyama', 'Mikasa Ackerman', 'Hori Kyouko', 'Miyamura_Izumi', 'Yuuki Asuna', 'Yukino_Yukinoshita', 'Misaki_Ayuzawa', 'Touka_Kirishima', 'Akatsuki no Yona Yona', 'Inori_Yuzuriha', 'Misa Amane', 'Historia Reiss', 'Chise Hatori', 'Izaya Orihara', 'Celty Sturluson', 'Rin Tousaka', 'Lawliet', 'Lelouch', 'Mitsuha','Ciel Phantomhive', 'Yuna Gasai', 'Nana', 'Shiro', 'Hiyori', 'Holo spice and wolf','Kaori Miyazono']
 
 def find_anime_character_url(character_name):
 	url = 'https://www.google.com/search?q=' + urllib.quote(character_name)
@@ -19,9 +19,12 @@ def find_anime_character_url(character_name):
 	for link in results:
 		if 'wikia' in link['href']: #/url?q=
 			return link['href'][7:].split('&', 1)[0]
-			break
+	
+	return ''
 
-
+def clean_data_string(data):
+	regex = re.compile(".*?\((.*?)\)")
+	return re.sub(r'\[.*\]', '', data).strip('\n')
 
 def scrape_weight(url):
 	regex = re.compile(".*?\((.*?)\)")
@@ -38,9 +41,9 @@ def scrape_weight(url):
 	values = soup.find("div", {"class" : "pi-data-value"})
 	for i, label in enumerate(labels):
 		if "weight" in label.text.lower():
-			return_data["weight"] = label.findNext('div').text
+			return_data["weight"] = clean_data_string(label.findNext('div').text)#.strip('\n')
 		if "height" in label.text.lower():
-			height = re.sub(r'\[.*\]', '', label.findNext('div').text)
+			height = clean_data_string(label.findNext('div').text)#re.sub(r'\[.*\]', '', label.findNext('div').text).strip('\n')
 			return_data["height"] = height
 
 	if "weight" not in return_data or "height" not in return_data:
@@ -48,9 +51,9 @@ def scrape_weight(url):
 		for td in td_labels:
 			#print td.text
 			if "weight" in td.text.lower():
-				return_data["weight"] = td.findNext('td').text
+				return_data["weight"] = clean_data_string(td.findNext('td').text.strip('\n'))
 			if "height" in td.text.lower():
-				return_data["height"] = td.findNext('td').text
+				return_data["height"] = clean_data_string(td.findNext('td').text)
 
 
 	return return_data
@@ -61,8 +64,13 @@ def write_to_file(data, filename):
 
 
 if __name__ == '__main__':
+	data = []
 	for character in characters:
-		print find_anime_character_url(character)
+		url = find_anime_character_url(character)
+		print url
+		if url:
+			data.append(scrape_weight(url))
+	write_to_file(data, "anime_character_stats")
 	# data = []
 	# for url in anime_urls:
 	# 	print url
