a0ec414098d72f639c0507bce5e50a0ceddbfe90
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-04-26 20:17:29 -0400

    Add zero width space pad

--- anonMail.py
+++ anonMail.py
@@ -1,7 +1,13 @@
 import requests
 
+
 POST_URL = 'http://anonymouse.org/cgi-bin/anon-email.cgi'
 #POST_BODY = {'to':None, 'subject':None, 'text':None}
+
+# Messages with same content can only be sent once, so make it different with 0 width space
+def pad_zero_width(text, num):
+	return text + num*'\u200b'
+
 
 def send(subject, message, receiver):
 	POST_BODY = {'to':receiver, 'subject':subject, 'text':message}
@@ -12,14 +18,14 @@ def send(subject, message, receiver):
 def go_crazy(subject, message, receiver, num=100): 
 	if isinstance(subject, str) and isinstance(message, str) and isinstance(receiver, str):
 		for i in range(num):
-			send(subject, message, receiver)
+			send(subject, pad_zero_width(message, i), receiver)
 	elif isinstance(subject, list) and isinstance(message, list): 
 		for i in range(num):
 			if i < len(subject):
 				print(i)
-				send(subject[i] + ' ' + str(i), message[i], receiver)
+				send(subject[i] + ' ' + str(i), pad_zero_width(message[i], i), receiver)
 	else:
 		print('Can\'t go crazy')
 
 if __name__ == '__main__':
-	go_crazy('List test', 'go crazy test', '<EMAIL HERE>', 10)
+	go_crazy('Pad test', 'try again', '<EMAIL HERE>', 5)
