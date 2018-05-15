1c8b40607c2927ea7d2b2af5db7046502ff58e68
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-05-14 21:25:37 -0400

    tokenize

--- imessage_analyzer.py
+++ imessage_analyzer.py
@@ -1,21 +1,26 @@
 import subprocess
 import sqlite3
+import sys
+import nltk
 
+# contact_info: phone number (ie. +19999999999) or apple id
 def retrieve_texts(contact_info):
-	con = sqlite3.connect("/Users/lucyzhang/Library/Messages/chat.db")
-	results = con.execute("select is_from_me,text from message where handle_id=("+
-		"select handle_id from chat_handle_join where chat_id=("+
-		"select ROWID from chat where guid='iMessage;-;" + contact_info + "')"+
-		")")
-	for result in results:
-		print(result)
-
-	# subprocess.run("sqlite3 ~/Library/Messages/chat.db \""+
-	# "select is_from_me,text from message where handle_id=("+
-	# "select handle_id from chat_handle_join where chat_id=("+
-	# "select ROWID from chat where guid='iMessage;-;" + contact_info + "')"+
-	# ")\" | sed 's/1\|/me: /g;s/0\|/budy: /g' > MessageBackup.txt", stdout=subprocess.PIPE)
+    con = sqlite3.connect("/Users/lucyzhang/Library/Messages/chat.db")
+    results = con.execute("select is_from_me,text from message where handle_id=("+
+        "select handle_id from chat_handle_join where chat_id=("+
+        "select ROWID from chat where guid='iMessage;-;" + contact_info + "')"+
+        ")")
+    for result in results:
+        # Your index is 1, the other person's index is 0
+        sender_index, message = result
+        tokens = nltk.word_tokenize(message)
+        print(tokens)
+        if sender_index is 0:
+            # do something with your own texts
+            continue
+        else:# do something with other person's texts
+            continue
 
 
 if __name__ == '__main__':
-	retrieve_texts("spothorse9.lucy@gmail.com")
+    retrieve_texts(sys.argv[1])
