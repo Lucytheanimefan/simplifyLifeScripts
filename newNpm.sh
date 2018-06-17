66b4262aba336093e75cff7b1d947542968a671e
Bill Yu <billyuhan@gmail.com>
2018-06-17 21:17:12 +0800

    one more thing

--- newNpm.sh
+++ newNpm.sh
@@ -3,5 +3,5 @@ mkdir $1
 cd $1
 cp -a ${dir}/assets/newNpm/. ./
 npm init -y
-npm i --save-dev eslint eslint-config-google nodemon babel-eslint
+npm i --save-dev cross-env eslint eslint-config-google nodemon babel-eslint
 git init
