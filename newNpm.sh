146f80eea0f60d04ba395222c63db726b034b254
Bill Yu <billyuhan@gmail.com>
2018-06-17 21:22:50 +0800

    ok I'm dumb

--- newNpm.sh
+++ newNpm.sh
@@ -3,5 +3,5 @@ mkdir $1
 cd $1
 cp -a ${dir}/assets/newNpm/. ./
 npm init -y
-npm i --save-dev babel-plugin-transform-object-rest-spread cross-env eslint eslint-config-google nodemon babel-eslint
+npm i --save-dev babel-preset-env babel-plugin-transform-object-rest-spread cross-env eslint eslint-config-google nodemon babel-eslint
 git init
